ddcc81ca217280ee9f63fce6110f6d398bd3fa35
Michael Obeng <michaelsquireobeng@gmail.com>
2021-02-19 16:04:21 +0000

    added stocklist array

---
+++ src/Stocklist.js
@@ -0,0 +1,20 @@
+const Stocklist = [
+    
+    {
+        name: 'Breathe',
+        price: 5,
+        quantityLeft: 44
+    },
+    {
+        name: 'His Cologne',
+        price: 3,
+        quantityLeft: 94
+    },
+    {
+        name: 'Fresh Laundry',
+        price: 4,
+        quantityLeft: 22
+    }
+];
+
+export default Stocklist
