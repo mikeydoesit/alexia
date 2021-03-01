08603887501f265cb6817b4d24a59ce44159ca74
Michael Obeng <michaelsquireobeng@gmail.com>
2021-03-01 17:53:15 +0000

    optimization

--- src/Scents.js
+++ src/Scents.js
@@ -3,42 +3,42 @@ const Scents = [
         name: 'Breathe',
         color: 'rgb(134,134,100)',
         logoURL: 'images/breathe.png',
-        imageURLs: ['images/breathe1.jpg','images/breathe2.png','images/breathe3.png','images/breathe4.png'],
+        imageURLs: ['images/breathe1.jpeg','images/breathe2.png','images/breathe3.png','images/breathe4.png'],
         description: "The scent was inspired by the popular 'Bath & Body Works' candle 'Eucalyptus and Spearmint'. I bought this candle on my trip to New York 9 years ago, and I've been completely obsessed."
     },
     {
         name: 'His Cologne',
         color: 'rgb(141,124,194)',
         logoURL: 'images/hisCologne.png',
-        imageURLs: ['images/hisCologne1.jpg','images/hisCologne2.png','images/hisCologne3.png','images/hisCologne4.png','images/hisCologne5.png','images/hisCologne6.png'],
+        imageURLs: ['images/hisCologne1.jpeg','images/hisCologne2.png','images/hisCologne3.png','images/hisCologne4.png','images/hisCologne5.png','images/hisCologne6.png'],
         description: "Do you ever catch yourself sniffing your man because he's wearing one of his sexy colognes and he smells absolutely amazing? Cos I do! All the time! Men's colognes are just *sniffs* so sexy! Now your whole house can smell like his cologne!"
     },
     {
         name: 'Fresh Laundry',
         color: 'rgb(224,102,104)',
         logoURL: 'images/freshLaundry.png',
-        imageURLs: ['images/freshLaundry1.jpg','images/freshLaundry2.png','images/freshLaundry3.png','images/freshLaundry4.png','images/freshLaundry5.png','images/freshLaundry5.png'],
+        imageURLs: ['images/freshLaundry1.jpeg','images/freshLaundry2.png','images/freshLaundry3.png','images/freshLaundry4.png','images/freshLaundry5.png','images/freshLaundry5.png'],
         description: "That feeling when you have fresh laundry and then everything just feels and smells amazing! It's not too overpowering but perfect enough to give off an amazingly warm, clean scent."
     },
     {
         name: 'Cocoa Butter Baby',
         color: 'rgb(150,128,105)',
         logoURL: 'images/cocoaButterBaby.png',
-        imageURLs: ['images/cocoaButterBaby1.jpg','images/cocoaButterBaby2.png','images/cocoaButterBaby3.png','images/cocoaButterBaby4.png','images/cocoaButterBaby5.png','images/cocoaButterBaby6.png'],
+        imageURLs: ['images/cocoaButterBaby1.jpeg','images/cocoaButterBaby2.png','images/cocoaButterBaby3.png','images/cocoaButterBaby4.png','images/cocoaButterBaby5.png','images/cocoaButterBaby6.png'],
         description: "Cocoa Butter Baby - Who doesn't love the smell of cocoa butter? Cocoa Butter fragrance oil releases rich and sweet, dark chocolate-like aromas, reminiscent of cocoa butter. Rich, warm and sweet. Perfect melt to create a cosy and warm environment."
     },
     {
         name: 'Cosy Home',
         color: 'rgb(70,128,130)',
         logoURL: 'images/cosyHome.png',
-        imageURLs: ['images/cosyHome1.jpg','images/cosyHome2.png','images/cosyHome3.png','images/cosyHome4.png','images/cosyHome5.png'],
+        imageURLs: ['images/cosyHome1.jpeg','images/cosyHome2.png','images/cosyHome3.png','images/cosyHome4.png','images/cosyHome5.png'],
         description: "In the words of Teddy Pendergrass... 'Turn down the lights and light a candle'. Put on your jim-jams, grab your favourite blanket and get nice and cosy!"
     },
     {
         name: 'Date Night',
         color: 'rgb(65,47,107)',
         logoURL: 'images/dateNight.png',
-        imageURLs: ['images/dateNight1.jpg','images/dreams1.png','images/dreams2.png','images/dreams3.png'],
+        imageURLs: ['images/dateNight1.jpeg','images/dreams1.png','images/dreams2.png','images/dreams3.png'],
         description: "Glossier polaroid meh copper mug woke enamel pin austin tote bag lumbersexual man braid trust fund kombucha. Bushwick try-hard beard church-key, etsy humblebrag ethical sriracha gentrify man bun jean shorts plaid VHS."
     },
     {
